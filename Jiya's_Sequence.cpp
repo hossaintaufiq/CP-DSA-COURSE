bb0a40779461a159ca3320ff4a2e496f5e0fc706
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-01-18 18:46:58 +0600

    2 problem solved

--- Jiya's_Sequence.cpp
+++ Jiya's_Sequence.cpp
@@ -9,12 +9,22 @@ int main(){
 	while(t--){
 		int n; 
 		cin>>n; 
+		long long int product=1;
+		for(int i=0;i<n;i++){
+			int x; 
+			cin>>x;
+			product*=x;
 
-		string s;
-		
+		}
 
-		getline(cin,s);
+		if(product%10==2||product%10==3||product%10==5){
+			cout<<"YES"<<endl;
+		}
+		else{
+			cout<<"NO"<<endl;
+		}
 
-		cout<<s;
+
+		// cout<<product;
 	}
 }
