62fbddab244ec78d2b6d0871e252e60909ab9e50
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-01-24 12:35:20 +0600

    time complexity

--- Time_Complexcity.cpp
+++ Time_Complexcity.cpp
@@ -1,6 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std; 
 
+// int main(){
+// 	int x ;
+// 	int sum=0;
+
+// 	sum=x+x;
+// 	// number of itreation 3 
+// 	// o(3);
+// }
+
+
 int main(){
+	int x ;// itreation 1 
+	int sum =0; // iteration 2 
+	sum = x+x ; 
+
+	int n; 
+	cin>> n ; //iteration 5 
+
+	for(int i ; i<n;++i){
+		// n time iteration 
+	} 
+
+
+	// total itreation =O(5)+O(n)=O(n);
 	
 }
