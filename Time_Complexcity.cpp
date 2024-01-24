fd4d616f035aae66e6814a5eda664e9b0f1e9431
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-01-24 15:06:14 +0600

    pre computation

--- Time_Complexcity.cpp
+++ Time_Complexcity.cpp
@@ -9,21 +9,54 @@ using namespace std;
 // 	// number of itreation 3 
 // 	// o(3);
 // }
-
+const int N = 10e5+10;
+long long int fact[N];
 
 int main(){
-	int x ;// itreation 1 
-	int sum =0; // iteration 2 
-	sum = x+x ; 
+	// int x ;// itreation 1 
+	// int sum =0; // iteration 2 
+	// sum = x+x ; 
 
-	int n; 
-	cin>> n ; //iteration 5 
+	// int n; 
+	// cin>> n ; //iteration 5 
 
-	for(int i ; i<n;++i){
-		// n time iteration 
-	} 
+	// for(int i ; i<n;++i){
+	// 	// n time iteration 
+	// } 
 
 
 	// total itreation =O(5)+O(n)=O(n);
-	
+
+	// int 0!=1!;
+
+
+	// int n ; 
+	// cin>> n ; 
+
+	// long long factorial =1; 
+
+	// for( int i =2;i <=n; ++i){
+	// 	factorial*=i; 
+
+	// }
+
+	// cout<< factorial<< endl ;
+
+
+	//  factorial with hashing 
+
+
+	fact[0]=fact[1]=1; 
+
+	for( int i=2; i<N; ++i){
+		fact[i]=fact[i-1]*i;
+	}
+
+	int t; 
+	cin>>t;
+	while (t--){
+		int n ; 
+		cin>>n ; 
+		cout<<fact[n]<<endl ;
+	}
 }
