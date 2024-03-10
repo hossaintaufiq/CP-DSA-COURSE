be99c75d246b54fe7ea8d93be1a986a178b78342
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-03-10 15:26:04 +0600

    lead code

--- hashing.cpp
+++ hashing.cpp
@@ -1,3 +1,8 @@
 // time complexity and space complexity
 
 // basic cpp and problems
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){
+	
+}
---
+++ swap_first_last_digits.cpp
@@ -0,0 +1,18 @@
+// swapint the fist ans last digits of a number ;
+
+
+#include<bits/stdc++.h>
+
+
+using namespace std ; 
+
+int main (){
+	int n; 
+	cin>>n;  
+
+	// log10(n) = total number of digits -1; 
+
+	int digits= (int)log10(n);
+
+	cout<<digits;
+}
