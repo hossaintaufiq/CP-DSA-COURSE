0f8b9a227a283a90498e28e353e7fd730dd58fe4
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-03-10 18:54:44 +0600

    code forces problem

--- swap_first_last_digits.cpp
+++ swap_first_last_digits.cpp
@@ -1,34 +1,49 @@
 // question 61: swapint the fist ans last digits of a number ;
 
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
 
+#define LIMIT 1000000
+long long i, j;
+long long prime_flag[LIMIT];
+void calculate_prime_flag(){
+    prime_flag[0] = prime_flag[1] = 1;
+    for(i=2;i<LIMIT;i++){
+        if (prime_flag[i]==0){
+            for(j=i*i;j<LIMIT;j+=i){
+                prime_flag[j] = 1;
+            }
+        }
+    }
+}
+int check_perfect_square(long long n){
+    double sqrt_n = sqrt(n);
+    if(sqrt_n == int(sqrt_n)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
 
-using namespace std ; 
-
-
-// 	class employee{
-// 	public: 
-// 		string name; 
-// 		int salary; 
-
-// 	}; 
-
-
-int main (){
-	int n; 
-	cin>>n;  
-
-	// log10(n) = total number of digits -1; 
-
-	int digits= (int)log10(n);
-
-	cout<<digits<<endl;
-
-	string mySql="Hello";
-
-	cout<<mySql[1]<<mySql[2]; 
-
-
-
+int main(){
+    calculate_prime_flag();
+    long long total_numbers, n;
+    cin>>total_numbers;
+    for(i=0;i<total_numbers;i++){
+        cin>>n;
+        if (n==4){ // only 4 is a even no which is a square of a prime number
+            cout<<"YES"<<endl;
+        }
+        else if (n%2==0){ //divisible by 2 
+            cout<<"NO"<<endl;
+        }
+        else if(check_perfect_square(n)==1 && prime_flag[int(sqrt(n))]==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
 }
