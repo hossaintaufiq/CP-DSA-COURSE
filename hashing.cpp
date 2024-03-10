0e3014aa6ba6a8cd14a4f4433e050adf90380d90
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-03-10 11:06:08 +0600

    leadcode problem

--- hashing.cpp
+++ hashing.cpp
@@ -40,3 +40,37 @@ int main(){
 	cout<<"\n";
 
 }
+
+
+
+
+// leadcodde problem 
+
+
+// class Solution {
+// public:
+//     string addBinary(string a, string b) {
+//         sting res; 
+//         int i =a.size()-1, j=b.size()-1;
+//         int sum, carry=0;
+
+//         while(i>=0 || j>=0){
+//             sum=carry; 
+//             if(i>=0){
+//                 sum+= a[i] -'0';
+//             }
+//             if(j>=0){
+//                 sum+= b[j] -'0';
+//             }
+//             res+= to_string(sum%2);
+//             carry= sum/2;
+//             i--;j--;
+
+//         }
+
+//         if(carry!=0) res+='1';
+
+//         reverse(res.begin(), res.end());
+//         return res;
+//     }
+// };
