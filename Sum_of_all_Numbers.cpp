f23682e42fbf743a3e87b2b6efde8508f0561301
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-03-11 19:40:16 +0600

    tranform function

--- Sum_of_all_Numbers.cpp
+++ Sum_of_all_Numbers.cpp
@@ -1,13 +1,20 @@
+// problem 68  
+
 #include<bits/stdc++.h>
 using namespace std; 
 
 int main(){
-	int x,y; 
 
-	cin>>x; 
+	string text; 
 
-	y=(x*(x+1))/2;
-	cout<<y;
+	// getting a line of text input from the user 
+	getline(cin,text);
+
+
+// using transfer function to convert each character of the string 
+	transform(text.begin(),text.end(),text.begin(),::toupper);
+
+	cout<<text<<endl;
 
 
 }
