03b38eec57a1bc7db64187bf195f5055ddfc41b2
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-03-13 23:47:20 +0600

    contest 1

---
+++ ali_baba.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+int main(){
+
+	long int x,y,z;
+	long long int d;
+
+	if(x*y*z= d || (x*y)+z=d || (x*y)z=d || (x+y)+z=d || (x-y)*z=d || x+(y*z)=d || x-(y*z)=d || x+y+z=d || (x+y)-z)
+
+
+}
--- ph_problems.cpp
+++ ph_problems.cpp
@@ -3,21 +3,16 @@
 using namespace std; 
 
 int main(){
-	long long int a,b,k;
+	char x; 
+	cin>>x; 
 
-	cin>>a>>b>>k;
+	if(x>='a' && x<='y'){
+		x++;
+	}
 
-
-	if(a%k==0 && b%k==0){
-		cout<<"Both";
+	else if(x='z'){
+		x='a';
 	}
-	else if (a%k==0 && b%k!=0){
-		cout<<"Memo";
-	}
-	else if (b%k==0 && a%k!=0){
-		cout<<"Momo";
-	}
-	else{
-		cout<<"No One";
-	}
+	
+	cout<<x; 
 }
