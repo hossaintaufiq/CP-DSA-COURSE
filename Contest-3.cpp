47b8446f70db94606cef65e9894c5d2e3405e064
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-03-25 16:00:53 +0600

    codeforces

--- Contest-3.cpp
+++ Contest-3.cpp
@@ -1,28 +1,10 @@
-#include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-  int x; 
-  cin>>x; 
-  int a[x];
-  for(int i=0;i<x; i++){
-    cin>>a[i];
-  }
-
-
-  for(int i=0;i<x; i++){
-    
-    if(a[i]>0){
-        a[i]= 1;
-    }
-    else if(a[i]<0){
-        a[i]=2;
-    }
-  }
-
-
-  for(int i=0; i<x; i++){
-    cout<<a[i]<<" ";
-  }
+int main()
+{
+long long i,n,f=0; cin>>n;
+if(n%2==0){  cout<<n/2;}
+else
+cout<<(-1*(n+1)/2);
+    return 0;
 }
