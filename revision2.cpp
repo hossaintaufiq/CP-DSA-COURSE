196965a8e502e16730b228f564c036c3aedc11be
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-04-23 01:48:55 +0600

    oop and prithon

--- revision2.cpp
+++ revision2.cpp
@@ -1,11 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std; 
 
+#define LOG(x) cout<<x<<endl;
+
 int main(){
-	int x; 
-	cin>>x;
+	// int x; 
+	// cin>>x;
 
-	cout<<exp(x)<<endl;
+	// cout<<exp(x)<<endl;
 
-	cout<<acos(x)<<endl;
+	// cout<<acos(x)<<endl;
+
+	// function overloading , recursion , pointers 
+	int var=8; 
+
+
+	void*ptr =&var;
+	cout<<cin.get(); 
+
+
 }
